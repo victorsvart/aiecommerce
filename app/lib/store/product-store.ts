a9d1e287acfeaad4a4cd471516f018a9a61f1751
Victor Moraes <victormoraespt4@gmail.com>
2025-08-02 04:01:30 -0300

    products seeding correction

---
+++ app/lib/store/product-store.ts
@@ -0,0 +1,16 @@
+export async function getProducts() {
+  return [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      description: "This is a product",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+      description: "This is another product",
+    },
+  ];
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,37 +1,70 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import fs from "fs";
 
 const prisma = new PrismaClient();
+const dataFolderPath = "./prisma/seedData/";
+
+async function seedProduct() {
+  try {
+    const data = fs.readFileSync(dataFolderPath + "products.json", "utf8");
+    const products: Prisma.ProductCreateInput[] = JSON.parse(data);
+
+    for (const product of products) {
+      const newProduct = await prisma.product.upsert({
+        where: { image: product.image },
+        update: {},
+        create: {
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          image: product.image,
+        },
+      });
+      console.log(`Product ${newProduct.name} created!`);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function seedUser() {
-  const user = await prisma.user.upsert({
-    where: { email: "svart@aiecommerce.com" },
-    update: {},
-    create: {
-      name: "Victor Moraes",
-      username: "victormoraes",
-      email: "svart@aiecommerce.com",
-      password: await bcrypt.hash(
-        process.env.SAMPLE_USER_PASSWORD as string,
-        10,
-      ),
-    },
-  });
+  try {
+    const user = await prisma.user.upsert({
+      where: { email: "svart@aiecommerce.com" },
+      update: {},
+      create: {
+        name: "Victor Moraes",
+        username: "victormoraes",
+        email: "svart@aiecommerce.com",
+        password: await bcrypt.hash(
+          process.env.SAMPLE_USER_PASSWORD as string,
+          10,
+        ),
+      },
+    });
 
-  console.log(`Sample user ${user.name} created or already exists!`);
+    console.log(`Sample user ${user.name} created or already exists!`);
+  } catch (err) {
+    throw err;
+  }
 }
 
 export async function main() {
-  console.log("hello")
-  await seedUser();
+  try {
+    await seedUser();
+    await seedProduct();
+  } catch (err) {
+    console.error("Seed failed:", err);
+  }
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
